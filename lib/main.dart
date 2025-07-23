bc6b3da7c47d7ea06003003abd1d50d5c74d4a26
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-07-23 08:50:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// Entry point
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('data'),
+        ),
+      ),
+    );
+  }
+}
