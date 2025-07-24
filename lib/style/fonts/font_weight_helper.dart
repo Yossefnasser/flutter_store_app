a575becba10a4b3085bfd593ef93e9abd90d19b5
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-07-24 08:42:25 +0300

    font family and change language button

---
+++ lib/style/fonts/font_weight_helper.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class FontWeightHelper {
+  const FontWeightHelper._();
+
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight meduim = FontWeight.w500;
+  static const FontWeight bold = FontWeight.w700;
+  
+}
