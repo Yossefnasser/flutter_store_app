c95c5d91824c38e5d1a09c4c6262a9d255e58911
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-07-26 04:17:51 +0300

    localization done

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pizza_app/core/app/common/screens/no_network_screen.dart';
 import 'package:pizza_app/core/app/connectivity_controller.dart';
 import 'package:pizza_app/core/extentions/context_extentions.dart';
+import 'package:pizza_app/core/language/app_localizations_setup.dart';
+import 'package:pizza_app/core/language/lang_keys.dart';
 import 'package:pizza_app/core/routes/app_routes.dart';
 import 'package:pizza_app/core/style/fonts/font_family_helper.dart';
 import 'package:pizza_app/core/style/fonts/font_weight_helper.dart';
@@ -31,6 +33,12 @@ class _MyAppState extends State<MyApp> {
           theme: themeLight(),
           darkTheme: themeDark(),
           debugShowCheckedModeBanner: false,
+          locale: Locale('en'),
+          supportedLocales: AppLocalizationsSetup.supportedLocales,
+          localizationsDelegates: AppLocalizationsSetup.localizationsDelegates,
+          localeResolutionCallback:
+              AppLocalizationsSetup.localeResolutionCallback,
+          title: 'Store App',
           initialRoute: AppRoutes.home,
           onGenerateRoute: AppRoutes.onGenerateRoute,
           home: isConnected ? const HomeScreen() : const NoNetworkScreen(),
@@ -64,56 +72,14 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    if (FontFamilyHelper.language == 'English')
-                      const Text(
-                        'Text AND Stuff',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: FontFamilyHelper.English,
-                          fontWeight: FontWeightHelper.bold,
-                          color: Colors.black,
-                        ),
-                      )
-                    else
-                      Text(
-                        'هذا هو نص تجريبي باللغة العربية',
-                        style: TextStyle(
-                          fontSize: 16.sp,
-                          fontFamily: FontFamilyHelper.Arabic,
-                          fontWeight: FontWeightHelper.bold,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                        ),
+                    Text(
+                      context.translate(LangKeys.appName),
+                      style:const TextStyle(
+                        fontSize: 20,
+                        fontFamily: FontFamilyHelper.English,
+                        fontWeight: FontWeightHelper.bold,
+                        color: Colors.black,
                       ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        var currentLanguage = FontFamilyHelper.getLanguage();
-                        print('Current language: $currentLanguage');
-                        String newlanguage;
-                        if (currentLanguage == 'English') {
-                          FontFamilyHelper.language = 'Arabic';
-                          newlanguage = FontFamilyHelper.getLanguage();
-                        } else {
-                          FontFamilyHelper.language = 'English';
-                          newlanguage = FontFamilyHelper.getLanguage();
-                        }
-                        print('New language: $newlanguage');
-                        setState(() {
-                          // Toggle language for demonstration
-                          FontFamilyHelper.language = newlanguage;
-                          print(
-                            'Language changed to: ${FontFamilyHelper.language}',
-                          );
-                        });
-                      },
-                      child: const Text(
-                        'change language',
-                        style: TextStyle(
-                          fontFamily: FontFamilyHelper.English,
-                          fontWeight: FontWeightHelper.regular,
-                        ),
-                      ),
-                      // Add more widgets here
                     ),
                     const SizedBox(height: 20),
                     ElevatedButton(
